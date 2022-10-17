203e4d6726abdcb0d633d720e53f8106a839f7d3
Toréa Patissier <p.torea@icloud.com>
2022-10-17 23:53:06 +0200

    feat: Part 1
    Add nodemon
    Add express
    Show the classic "Hello World"

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('ok')
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req,res)=> res.send('HELLO  WORLD'))
+
+app.listen(port,()=> console.log(`Notre app à démmarrée sur le port ${port}`))
