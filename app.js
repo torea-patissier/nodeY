792bd4254f37f079968c41ce5e9c84ecd1d95318
Torea Patissier <p.torea@icloud.com>
2022-10-20 11:00:51 +0200

    feat: Part 2
    dynamic routes on GET request

--- app.js
+++ app.js
@@ -4,4 +4,10 @@ const port = 3000;
 
 app.get('/', (req,res)=> res.send('HELLO  WORLD'))
 
-app.listen(port,()=> console.log(`Notre app à démmarrée sur le port ${port}`))
+app.get('/api/pokemons/:id',(req,res)=>{ // id dynamique
+    const id = req.params.id;
+    res.send(`Vous avez demandé le pokemon n ${id}`);
+});
+
+
+app.listen(port,()=> console.log(`Notre app à démmarrée sur le port ${port}`));
