7b15d8c81ed2474a9bc23292ea90843bce782f27
Torea Patissier <p.torea@icloud.com>
2022-10-20 19:18:17 +0200

    feat: Custom message
    helper.js

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 let pokemons = require('./mock-pokemon');
+const helper = require('./helper');
 
 app.get('/', (req,res)=> res.send('HELLO  WORLD'))
 
 app.get('/api/pokemons/:id',(req,res)=>{
     const id = parseInt(req.params.id)
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
-    res.send(`Vous avez trouvé le pokemon ${pokemon.name}.`);
+    const message = 'Pokémon trouvé'
+    res.json(helper.success(message,pokemon));
 })
 /**
  * parseInt or we get error because express transform id type int in string
@@ -18,8 +20,5 @@ app.get('/api/pokemons/:id',(req,res)=>{
 app.get('/api/pokemons',(req,res)=>{
     res.send(`Il y'a ${pokemons.length} pokémons dans la Bdd`)
 });
-/**
- * Get ALL
- */
 
-app.listen(port,()=> console.log(`Notre app à démmarrée sur le port ${port}`));
+app.listen(port,()=> console.log(`Notre app à démmarrée sur le port localhost:${port}`));
---
+++ helper.js
@@ -0,0 +1,14 @@
+exports.success  = (message, data) => {
+  return { message, data }
+}
+
+/**
+ * Like
+ * const success = (message,data) => {
+ *   return{
+ *     message: message,
+ *     data : data
+ *   }
+ * }
+ */
+
