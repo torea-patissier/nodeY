b48145e62d7ce95b2429a039116146e76d72afea
Torea Patissier <p.torea@icloud.com>
2022-10-20 11:52:24 +0200

    feat: Call external file and get an item from this file
    Dynamic GET routes
    Using parseInt

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+let pokemons = require('./mock-pokemon');
 
 app.get('/', (req,res)=> res.send('HELLO  WORLD'))
 
-app.get('/api/pokemons/:id',(req,res)=>{ // id dynamique
-    const id = req.params.id;
-    res.send(`Vous avez demandé le pokemon n ${id}`);
-});
-
+app.get('/api/pokemons/:id',(req,res)=>{
+    const id = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    res.send(`Vous avez trouvé le pokemon ${pokemon.name}.`);
+})
+/**
+ * parseInt or we get error because express transform id type int in string
+ * pokémon id == id pass in URL
+ */
 
 app.listen(port,()=> console.log(`Notre app à démmarrée sur le port ${port}`));
