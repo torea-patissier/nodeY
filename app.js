e0cfb3fd9b9447aa59c8b2006853050f35c0cfe4
Torea Patissier <p.torea@icloud.com>
2022-10-22 16:34:03 +0200

    feat: Create a DELETE route

--- app.js
+++ app.js
@@ -12,42 +12,50 @@ app
    .use(morgan('dev'))
    .use(bodyParser.json()) // Pour parser toutes les données envoyés vers l'API
 
-app.get('/',(req,res)=>res.send('Hi again'))
+app.get('/', (req, res) => res.send('Hi again'))
 
-app.get('/api/pokemons/:id',(req,res)=>{
-    const id = parseInt(req.params.id)
-    const pokemon = pokemons.find(pokemon => pokemon.id === id)
-    const message = 'Pokémon trouvé'
-    res.json(helper.success(message,pokemon));
+app.get('/api/pokemons/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const pokemon = pokemons.find(pokemon => pokemon.id === id)
+  const message = 'Pokémon trouvé'
+  res.json(helper.success(message, pokemon));
 })
 
-app.get('/api/pokemons',(req,res)=>{
-    const message = 'Tous les pokémons sont ici : '
-    res.json(helper.success(message,pokemons));
+app.get('/api/pokemons', (req, res) => {
+  const message = 'Tous les pokémons sont ici : '
+  res.json(helper.success(message, pokemons));
 });
 
-app.post('/api/pokemons',(req,res) => {
-    const id = helper.getUniqueId(pokemons);
-    const pokemonCreated = {...req.body,... { id: id, created: new Date()} }
-    /**
-     * On ajoute dans pokemonCreated
-     * req.body = le body du formulaire
-     * id = l'id créé à la mano
-     * created new date = la date
-     */
-    pokemons.push(pokemonCreated)
-    const message = `Le pokémon ${pokemonCreated.name} à bien été créé!`;
-    res.json(helper.success(message, pokemonCreated));
+app.post('/api/pokemons', (req, res) => {
+  const id = helper.getUniqueId(pokemons);
+  const pokemonCreated = {...req.body, ...{id: id, created: new Date()}}
+  /**
+   * On ajoute dans pokemonCreated
+   * req.body = le body du formulaire
+   * id = l'id créé à la mano
+   * created new date = la date
+   */
+  pokemons.push(pokemonCreated)
+  const message = `Le pokémon ${pokemonCreated.name} à bien été créé!`;
+  res.json(helper.success(message, pokemonCreated));
 })
 
-app.put('/api/pokemons/:id',(req,res) => {
+app.put('/api/pokemons/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const pokemonUpdated = {...req.body, id: id}
+  pokemons = pokemons.map(pokemon => {
+    return pokemon.id === id ? pokemonUpdated : pokemon
+  })
+  const message = `Le pokémon ${pokemonUpdated.name} à bien été modifié !`
+  res.json(helper.success(message, pokemonUpdated))
+})
+
+app.delete('/api/pokemons/:id', (req, res) => {
     const id = parseInt(req.params.id)
-    const pokemonUpdated = { ...req.body, id: id }
-    pokemons = pokemons.map(pokemon => {
-        return pokemon.id === id ? pokemonUpdated : pokemon
-    })
-    const message = `Le pokémon ${pokemonUpdated.name} à bien été modifié !`
-    res.json(helper.success(message, pokemonUpdated))
+    const pokemonDeleted = pokemons.find(pokemon => pokemon.id === id)
+    pokemons  = pokemons.filter(pokemon => pokemon.id !== id)
+    const message = `Le pokémon ${pokemonDeleted.name} à bien été supprimé !`
+    res.json(helper.success(message,pokemonDeleted))
 })
 
-app.listen(port,()=> console.log(`Notre app à démmarrée sur le port localhost:${port}`));
+app.listen(port, () => console.log(`Notre app à démmarrée sur le port localhost:${port}`));
